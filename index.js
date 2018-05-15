3c4ff8eec41a54160f4e4077f7013707c1c630b9
Georgios Anastasiou <gsiou@gunet.gr>
2018-05-15 14:36:58 +0300

    Mailchimp - Add subscriber

---
+++ index.js
@@ -0,0 +1,76 @@
+const fetch = require("node-fetch");
+const base64 = require("base-64");
+const crypto = require("crypto");
+
+class Newsletter {
+  
+  constructor(config) {
+    this.config = config;
+  }
+
+  async addSubscriber(listId, data, preventSpam = false) {throw new Error('Not implemented');}
+
+  async removeSubscriber(listId, email) {throw new Error('Not implemented');}
+
+  async getList(listId) {throw new Error('Not implemented');}
+
+  static getMD5(email) {
+    return crypto.createHash("md5").update(email).digest("hex");
+  }
+}
+
+class MailchimpWrapper extends Newsletter {
+  constructor(config) {
+    super(config);
+  }
+
+  async addSubscriber(listId, data, preventSpam = false) {
+    if (preventSpam) {
+      // Check if user is already 'pending' or 'subscribed' to the list
+      const resp = await fetch(
+        this.config.newsletterBase +
+          "/lists/" +
+          listId +
+          "/members/" +
+          getMD5(data.email),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Basic " + base64.encode("api:" + this.config.newsletterToken)
+          }
+        }
+      );
+      const json = await resp.json();
+      if (json.status === "pending") {
+        return json;
+      }
+    }
+    const response = await fetch(
+      this.config.newsletterBase +
+        "/lists/" +
+        listId +
+        "/members/" +
+        Newsletter.getMD5(data.email),
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + base64.encode("api:" + this.config.newsletterToken)
+        },
+        body: JSON.stringify({
+          email_address: data.email,
+          status: data.confirmation ? "pending" : "subscribed",
+          merge_fields: {
+            FNAME: data.firstName,
+            LNAME: data.lastName
+          }
+        })
+      }
+    );
+    const responseJson = await response.json();
+    return responseJson;
+  }
+}
+
+module.exports.MailchimpWrapper = MailchimpWrapper;
